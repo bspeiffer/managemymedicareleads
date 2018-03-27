d913b74c0394b9302bab823bd0dd6d666ffae8b6
Brent Peiffer <bspeiffer@gmail.com>
2018-03-26 21:40:38 -0400

    json package edits

--- scripts/loadtable.js
+++ scripts/loadtable.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    alert("Loaded");
-});
+$(document).ready(function () {
+  alert('Loaded')
+})
 
 // function show() {
 
@@ -103,13 +103,13 @@ $(document).ready(function() {
 
 // 	    // 1. remove all existing rows
 //     $("tr:has(td)").remove();
- 
+
 //     // 2. get each article
 //     $.each(leads, function (i,lead) {
- 
+
 //         // 2.2 Create table column for categories
 //         var td_friends = $("<td/>");
- 
+
 //         // 2.3 get each category of this article
 //         $.each(lead.friends, function (i, friend) {
 //         	$.each(friend.name, function(i, name) {
@@ -118,78 +118,84 @@ $(document).ready(function() {
 //             td_friends.append(span);
 //         	});
 //         });
- 
+
 //         // 2.4 Create table column for tags
 //        var td_tags = $("<td/>");
- 
-//         // 2.5 get each tag of this article     
+
+//         // 2.5 get each tag of this article
 //         $.each(leads.tags, function (i, tag) {
 //             var span = $("<span class='label' style='margin:4px;padding:4px' />");
 //             span.text(tag);
 //             td_tags.append(span);
 //         });
- 
+
 //         // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
 //         $("#added-articles").append($('<tr/>')
 //                 .append($('<td/>').html("<a href='"+lead.picture+"'>"+lead.name+"</a>"))
 //                 .append(td_friends)
 //                 .append(td_tags)
-//         );  
-//     }); 
+//         );
+//     });
 
 // }
 
-//Working example
+// Working example
 
-function show() {
- 
-    // JSON Data
-    var articles = [
-        { 
-            "title":"jQuery.each(JSON)",
-            "url":"http://hmkcode.com/jquery-each-json/",
-            "categories":["jQuery"],
-            "tags":["jquery","json","$.each"]
-        },
-        {
-            "title":" Java Servlet Send & Receive JSON Using jQuery.ajax() ",
-            "url":"http://hmkcode.com/java-servlet-send-receive-json-using-jquery-ajax/",
-            "categories":["Java"],
-            "tags":["java","json","jquery"]
-        }
-    ];
- 
-    // 1. remove all existing rows
-    $("tr:has(td)").remove();
- 
-    // 2. get each article
-    $.each(articles, function (index, article) {
- 
-        // 2.2 Create table column for categories
-        var td_categories = $("<td/>");
- 
-        // 2.3 get each category of this article
-        $.each(article.categories, function (i, category) {
-            var span = $("<span class='label label-info' style='margin:4px;padding:4px' />");
-            span.text(category);
-            td_categories.append(span);
-        });
- 
-        // 2.4 Create table column for tags
-       var td_tags = $("<td/>");
- 
-        // 2.5 get each tag of this article     
-        $.each(article.tags, function (i, tag) {
-            var span = $("<span class='label' style='margin:4px;padding:4px' />");
-            span.text(tag);
-            td_tags.append(span);
-        });
- 
-        // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
-        $("#leads-table").append($('<tr/>')
-                .append($('<td/>').html("<a href='"+article.url+"'>"+article.title+"</a>"))
-                .append(td_categories)
-                .append(td_tags)
-        );  
-    });             
+function show () {
+  // JSON Data
+  var articles = [
+    {
+      title: 'jQuery.each(JSON)',
+      url: 'http://hmkcode.com/jquery-each-json/',
+      categories: ['jQuery'],
+      tags: ['jquery', 'json', '$.each']
+    },
+    {
+      title: ' Java Servlet Send & Receive JSON Using jQuery.ajax() ',
+      url:
+        'http://hmkcode.com/java-servlet-send-receive-json-using-jquery-ajax/',
+      categories: ['Java'],
+      tags: ['java', 'json', 'jquery']
+    }
+  ]
+
+  // 1. remove all existing rows
+  $('tr:has(td)').remove()
+
+  // 2. get each article
+  $.each(articles, function (index, article) {
+    // 2.2 Create table column for categories
+    var td_categories = $('<td/>')
+
+    // 2.3 get each category of this article
+    $.each(article.categories, function (i, category) {
+      var span = $(
+        "<span class='label label-info' style='margin:4px;padding:4px' />"
+      )
+      span.text(category)
+      td_categories.append(span)
+    })
+
+    // 2.4 Create table column for tags
+    var td_tags = $('<td/>')
+
+    // 2.5 get each tag of this article
+    $.each(article.tags, function (i, tag) {
+      var span = $("<span class='label' style='margin:4px;padding:4px' />")
+      span.text(tag)
+      td_tags.append(span)
+    })
+
+    // 2.6 Create a new row and append 3 columns (title+url, categories, tags)
+    $('#leads-table').append(
+      $('<tr/>')
+        .append(
+          $('<td/>').html(
+            "<a href='" + article.url + "'>" + article.title + '</a>'
+          )
+        )
+        .append(td_categories)
+        .append(td_tags)
+    )
+  })
 }
